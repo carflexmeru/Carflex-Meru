fca12d3d0e85fe85e5d2f41aa3118f4e13eacf9e
kbrian1237 <kbrian1237@gmail.com>
2026-05-01 14:49:57 +0300

    style: updated hero mask to 100% opacity for solid-state stenciling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,8 +82,8 @@ export default function LandingPage() {
                     </text>
                  </mask>
               </defs>
-              {/* Darkened Tactical Grey Overlay (92% Opacity) */}
-              <rect width="1000" height="500" fill="#121212" fillOpacity="0.92" mask="url(#heroInversionMask)" />
+              {/* Solid Tactical Grey Overlay (100% Opacity) */}
+              <rect width="1000" height="500" fill="#121212" fillOpacity="1" mask="url(#heroInversionMask)" />
            </svg>
         </div>
 
